990a07fb7ff80917ab7702c117eccdea2b383ee8
Jacob Kato <jacobkato49@yahoo.com>
2017-08-13 20:30:28 -0700

    debugging schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-  id INT AUTO INCREMENT,
-  PRIMARY KEY(id),
+  id INT AUTO_INCREMENT,
   burgers_name varchar(100) NOT NULL,
   devoured BOOLEAN default false,
-  date TIMESTAMP
+  date TIMESTAMP,
+  PRIMARY KEY(id)
 );
