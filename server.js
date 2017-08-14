7bdc0f9b64d4c7b310bb3d4a8caa387ab18496d0
Jacob Kato <jacobkato49@yahoo.com>
2017-08-13 18:27:48 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+//npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app= express();
+
+//middleware
+app.use(express.static(process.cwd()+"/public")); /*Serve static content for the app from the "public" directory in the application directory*/
+
+/*returns middleware that only parses urlencoded bodies**/
+app.use(bodyParser.urlencoded({
+  extended: false /*parse extended syntax with the qs module*/
+}))
+
+//override with POST method
+app.use(methodOverride("_method"))
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+}));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+app.use("/", routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
